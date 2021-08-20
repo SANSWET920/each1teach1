800811d694753f551d378928353760f30b990200
SANSWET920 <ssanthanam242@gmail.com>
2021-08-20 06:30:54 +0530

    vsdvdfv

---
+++ src/data.js
@@ -0,0 +1,94 @@
+const products = [
+  {
+    _id: "1",
+    name: "Licensed Frozen Ball",
+    image:
+      "http://placeimg.com/640/480/animals",
+
+    price: 41070,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Sleek Concrete Salad",
+    image:
+      "http://placeimg.com/640/480/abstract",
+
+    price: 50883,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Handcrafted Steel Ball",
+    image:
+      "http://placeimg.com/640/480/cats",
+
+    price: 29170,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Handmade Frozen Bacon",
+    image:
+      "http://placeimg.com/640/480/sports",
+
+    price: 34536,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Gorgeous Rubber Car",
+    image:
+      "http://placeimg.com/640/480/city",
+
+    price: 31367,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Ergonomic Rubber Computer",
+    image:
+      "http://placeimg.com/640/480/sports",
+
+    price:32993,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: "1",
+    name: "Refined Soft Shoes",
+    image:
+      "http://placeimg.com/640/480/technics",
+
+    price: 33628,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Licensed Fresh Table",
+    image:
+      "http://placeimg.com/640/480/sports",
+
+    price: 52541,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Handcrafted Steel Table",
+    image:
+      "http://placeimg.com/640/480/sports",
+
+    price: 41345,
+    rating: 3,
+    numReviews: 12,
+  },
+];
+
+export default products;
